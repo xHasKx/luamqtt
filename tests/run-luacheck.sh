3c61c6b41237dd321be0cfd11454af3fae9d61ea
Alexander Kiranov <xhaskx@gmail.com>
2026-07-04 21:02:56 +0000

    run-luacheck.sh: also lint rockspec files

--- tests/run-luacheck.sh
+++ tests/run-luacheck.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+set -e
+
 luacheck examples/ mqtt/ tests/ tools/ $*
+
+# NOTE: luacheck treats .rockspec arguments as manifests and checks files from their build.modules,
+# so feed rockspecs via stdin to lint the rockspec code itself
+for f in ./*.rockspec rockspecs/*.rockspec; do
+	luacheck --std rockspec --filename "$f" $* - < "$f"
+done
